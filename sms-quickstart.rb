c232d7a0d70e2a1155a46d767de4785304ec3bf7
Hani Kazmi <hanikazmi@me.com>
2014-10-05 00:56:32 +0100

    Wikipedia integration

--- sms-quickstart.rb
+++ sms-quickstart.rb
@@ -1,11 +1,20 @@
 require 'rubygems'
 require 'twilio-ruby'
 require 'sinatra'
- 
+require 'HTTParty'
+
 get '/sms-quickstart' do
   sender = params[:Body]
+  response = HTTParty.get('http://en.wikipedia.org/w/api.php?action=query&prop=extracts&format=json&exintro=&titles=#{sender}&redirects')
+  d = JSON.parse(response.body).deep_find("extract")
   twiml = Twilio::TwiML::Response.new do |r|
-    r.Message "Hello, Hani. Thanks for the message #{sender}."
+    r.Message d
   end
   twiml.text
+end
+
+class Hash
+  def deep_find(key)
+    key?(key) ? self[key] : self.values.inject(nil) {|memo, v| memo ||= v.deep_find(key) if v.respond_to?(:deep_find) }
+  end
 end

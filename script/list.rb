55e345da67e4069083e1a32ec30667a02f8137be
Hani Kazmi <hanikazmi@me.com>
2014-10-05 11:16:06 +0100

    V1.0

---
+++ script/list.rb
@@ -0,0 +1,3 @@
+def list text
+  "w (text) : display wikipedia article\nfb (team) : Check team's position in the Premier League\nhn : Show the top post on Hacker News\npkmn (pokemon) : Show a pokedex entry"
+end

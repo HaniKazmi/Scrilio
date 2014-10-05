1819bb22b638179fe6486984e211d059d05436c2
Hani Kazmi <hanikazmi@me.com>
2014-10-05 09:15:39 +0100

    Add help

--- script/fb.rb
+++ script/fb.rb
@@ -1,6 +1,5 @@
 def fb text
   response = HTTParty.get(URI.encode("http://football-api.com/api/?Action=standings&APIKey=b89a5775-dcb4-a4d4-2ca9a2c98316&comp_id=1204"))
   p response
-  pos = JSON.parse(response.body)['teams'].each { |a| if a["stand_team_name"].downcase==text.downcase; return a["stand_position"] end }
-  "#{text} is #{pos} in the league"
+  pos = JSON.parse(response.body)['teams'].each { |a| if a["stand_team_name"].downcase==text.downcase; return "#{text} is #{a["stand_position"]} in the league" end }
 end
---
+++ script/help.rb
@@ -0,0 +1,3 @@
+def help
+  "w (text) : display wikipedia article\n fb (team) : Check team's position in the Premier League\n hn : Show the top post on Hacker News\n pkmn (pokemon) : SHow a pokedex entry"
+end

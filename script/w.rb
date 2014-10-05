b655a17390d2e03c23acd02a317812eb89d08c97
Hani Kazmi <hanikazmi@me.com>
2014-10-05 06:11:22 +0100

    Cleanup

--- script/w.rb
+++ script/w.rb
@@ -1,9 +1,9 @@
-def w sender
+def w text
   begin
-    response = HTTParty.get(URI.encode("http://en.wikipedia.org/w/api.php?action=query&prop=extracts&format=json&exintro=&titles=#{sender}&redirects"))
+    response = HTTParty.get(URI.encode("http://en.wikipedia.org/w/api.php?action=query&prop=extracts&format=json&exintro=&titles=#{text}&redirects"))
     d = JSON.parse(response.body).deep_find("extract")
     d.gsub(/<\/?..?>/, '')
   rescue
-    "#{sender} not found"
+    "#{text} not found"
   end
 end

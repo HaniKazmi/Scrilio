7351d6080c7ecb5064adabb7756b9cc0f0d1e78c
Hani Kazmi <hanikazmi@me.com>
2014-10-05 06:31:59 +0100

    Cleanup

--- script/fb.rb
+++ script/fb.rb
@@ -1,9 +1,9 @@
 def fb text
   begin
     response = HTTParty.get(URI.encode("http://football-api.com/api/?Action=standings&APIKey=b89a5775-dcb4-a4d4-2ca9a2c98316&comp_id=1204"))
+    p response
     d = JSON.parse(response.body)['teams']
     "#{text} is #{team_standing(d, text)} in the league"
-    p response
   rescue
     "#{text} is not found"
   end

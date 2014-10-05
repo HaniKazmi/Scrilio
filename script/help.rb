003218d17b969a61f6b8d857e5ebe6c7c9daf5ea
Hani Kazmi <hanikazmi@me.com>
2014-10-05 09:17:18 +0100

    Rename to helpme to avoid conflict

--- script/help.rb
+++ script/help.rb
@@ -1,3 +1,3 @@
-def help
+def helpme
   "w (text) : display wikipedia article\n fb (team) : Check team's position in the Premier League\n hn : Show the top post on Hacker News\n pkmn (pokemon) : SHow a pokedex entry"
 end
